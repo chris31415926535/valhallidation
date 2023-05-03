786a8181b1bca0aa46fa86ed905c9d2f98cc5e53
Christopher Belanger <christopher.a.belanger@gmail.com>
2023-05-03 19:11:09 +0000

    Adding 200 municipal addresses

--- _targets.R
+++ _targets.R
@@ -10,14 +10,18 @@ api_key <- readr::read_file(".apikey")
 set.seed(12345)
 
 list(
-  tar_target(num_to_sample, 200),
+  # tar_target(num_to_sample, 200),
+  #
+  # # input data of Ontario municipal address points, updated 11 Feb 2023
+  # # https://open.ottawa.ca/datasets/ottawa::municipal-address-points/about
+  # tar_target(create_addresses,
+  #            sf::read_sf("data/municipal_addresses/Municipal_Address_Points.shp") |>
+  #              dplyr::slice_sample(n = num_to_sample) |>
+  #              sf::write_sf("data/municipal_addresses_200.shp")
+  # ),
 
-  # input data of Ontario municipal address points, updated 11 Feb 2023
-  # https://open.ottawa.ca/datasets/ottawa::municipal-address-points/about
-  tar_target(addresses,
-             sf::read_sf("data/Municipal_Address_Points.shp") |>
-               dplyr::slice_sample(n = num_to_sample)),
-
+  tar_target(addresses_muni,
+             sf::read_sf("data/municipal_addresses_200.shp")),
 
 
 
